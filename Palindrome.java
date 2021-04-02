a9b4520af7dac161c1bb0b8ec4d6ee85d35d8ddc
NadirAlizada <>
2021-04-02 13:10:05 +0000

    code

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,18 @@
 public class Palindrome{
     public static boolean is_palindrome(String s)
     {
-        // add your code here
-        return true;
+    int i = 0, j = s.length() - 1;
+ 
+        // While there are characters to compare
+        while (i < j) {
+ 
+            // If there is a mismatch
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+ 
+            i++;
+            j--;
+    }
+    return true;
     }
 }
